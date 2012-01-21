e5cfd59447f6328d0d568ad122afd4f583af6cea
Luke Young <innoying@gmail.com>
2012-01-21 15:24:16 -0600

    Added Driving Code

--- src/org/inceptus/inceptusRobot.java
+++ src/org/inceptus/inceptusRobot.java
@@ -11,6 +11,7 @@ package org.inceptus;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 import org.inceptus.Constants;
 
 /**
@@ -27,10 +28,10 @@ public class inceptusRobot extends IterativeRobot {
     private Joystick armJoy;
     
     //Init the drive motors
-    private Jaguar leftRearDrive;
-    private Jaguar rightRearDrive;
-    private Jaguar leftFrontDrive;
-    private Jaguar rightFrontDrive;
+    private Jaguar rearLeftDrive;
+    private Jaguar frontLeftDrive;
+    private Jaguar rearRightDrive;
+    private Jaguar frontRightDrive;
     
     //Init the conveyor motors
     private Jaguar lowerConveyorDrive;
@@ -38,6 +39,9 @@ public class inceptusRobot extends IterativeRobot {
     
     //Init the shooting motor
     private Jaguar shootingWheelDrive;
+    
+    //Setup the mecanum drive
+    private RobotDrive drive;
     
     /**
      * This function is run when the robot is first started up and should be
@@ -49,15 +53,17 @@ public class inceptusRobot extends IterativeRobot {
         //Setup the arm joystick
         armJoy = new Joystick(Constants.armJoyPort); 
         //Setup the drive motors
-        leftRearDrive = new Jaguar(Constants.leftRearDrivePort);
-        rightRearDrive = new Jaguar(Constants.rightRearDrivePort);
-        leftFrontDrive = new Jaguar(Constants.leftFrontDrivePort);
-        rightFrontDrive = new Jaguar(Constants.rightFrontDrivePort);
+        rearLeftDrive = new Jaguar(Constants.leftRearDrivePort);
+        rearRightDrive = new Jaguar(Constants.rightRearDrivePort);
+        frontLeftDrive = new Jaguar(Constants.leftFrontDrivePort);
+        frontRightDrive = new Jaguar(Constants.rightFrontDrivePort);
         //Setup the Conveyor motors
         lowerConveyorDrive = new Jaguar(Constants.lowerConveyorDrivePort);
         upperConveyorDrive = new Jaguar(Constants.upperConveyorDrivePort);
         //Setup the shooting motor
         shootingWheelDrive = new Jaguar(Constants.shootingWheelDrivePort);
+        //Setup the drive
+        drive = new RobotDrive(rearLeftDrive, frontLeftDrive, rearRightDrive, frontRightDrive);
     }
 
     /**
@@ -71,7 +77,39 @@ public class inceptusRobot extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
+        //Init joystick values T=Throttle
+        double T, Z;
         
+        //Set the joystick values
+        T = driveJoy.getRawAxis(3);
+        Z = driveJoy.getRawAxis(4);
+        
+        //Init the values for each
+        double magnitude, rotation, vector;
+        //Use the distance formula to 
+        magnitude = driveJoy.getMagnitude();
+        //Get the rotation
+        vector = driveJoy.getDirectionDegrees();
+        //Set the vector to the twist from the Joystick
+        rotation = Z;
+        
+        //Check and use the thresholds
+        magnitude = (Math.abs(magnitude) < Constants.magnitudeThreshold) ? 0 : magnitude;
+        
+        //Adjust to 0-1 range
+        T = ((T+1)/2);
+     
+        //Scale the magnitude down to not overpower the motors
+        T = Constants.magnitudeMin + (T * (Constants.magnitudeMax - Constants.magnitudeMin));  
+        
+        //Use the throttle value to normalize the magnitude
+        magnitude = magnitude * (1-T);
+        
+        
+        //Drive mecanum using polar coordinates
+        drive.mecanumDrive_Polar(magnitude, vector, rotation);
+        
+        //Init the arm values
     }
     
 }
