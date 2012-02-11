fb281c0b7c90a8628f1b4a37274ecd101ac63ad8
Luke Young <innoying@mac.(none)>
2012-02-10 22:40:06 -0600

    Added log method

--- src/org/inceptus/debug/Debug.java
+++ src/org/inceptus/debug/Debug.java
@@ -16,6 +16,16 @@ public class Debug {
         //Print to console
         System.out.println( "(inceptus) FATAL Exception Message: " + msg );
         //Send message to driverstation
-        DriverStationLCD.getInstance().println( DriverStationLCD.Line.kUser3, 1, "FATAL:" + msg );
+        DriverStationLCD.getInstance().println( DriverStationLCD.Line.kUser3, 1, 
+                "FATAL:" + msg );
+    }
+    
+    //Log a message
+    public static void log(String msg){
+        //Output to console
+        System.out.println( "(inceptus) Log Message:" + msg );
+        //Send message to driverstation
+        DriverStationLCD.getInstance().println( DriverStationLCD.Line.kUser2, 1,
+                "Log:" + msg );
     }
 }
