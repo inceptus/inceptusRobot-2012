7e3d0b4b85c94702c984a1a5f78df91616aa1a47
Luke Young <innoying@mac.(none)>
2012-02-14 20:25:44 -0600

    Updated OI

--- src/org/inceptus/OI/OI.java
+++ src/org/inceptus/OI/OI.java
@@ -2,10 +2,12 @@ package org.inceptus.OI;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
+import org.inceptus.camera.Target;
 import org.inceptus.camera.TargetFinder;
 import org.inceptus.chassis.Drive;
 import org.inceptus.chassis.LowerConveyor;
 import org.inceptus.chassis.Ramp;
+import org.inceptus.chassis.UpperShooter;
 import org.inceptus.debug.Debug;
 
 
@@ -17,6 +19,9 @@ public class OI {
     //Joysticks
     private InJoystick mainJoy;
     private Joystick otherJoy;
+    
+    boolean lastButton1 = false;
+    boolean lastButton2 = false;
     
     public OI(){
         
@@ -43,19 +48,93 @@ public class OI {
     public void moveRamp(Ramp ramp) {
         
         //If the ramp button is pressed
-        if(mainJoy.getRawButton(5)){
+        if(mainJoy.getRawButton(1)){
+            //Call the down routine
             ramp.moveDown();
         }else{
+            //Call the up routine
             ramp.moveUp();
         }
         
     }
     
-    public void updateCamera(TargetFinder targetFinder){
+    public void runUpperShooter(UpperShooter upperShooter, TargetFinder targetFinder){
+        
+        //Calc buttons from the POV hat
+        boolean button1 = (otherJoy.getRawAxis(6) == 1);
+        boolean button2 = (otherJoy.getRawAxis(6) == -1);
+        
+        //If changed
+        if(button1 != lastButton1){
+            
+            //Adjust offset up
+            upperShooter.adjustOffset(true);
+            
+        }
+        
+        //If changed
+        if(button2 != lastButton2){
+            
+            //Adjust offset down
+            upperShooter.adjustOffset(false);
+            
+        }
+        
+        //Reset "lastButton"
+        lastButton1 = button1;
+        lastButton2 = button2;
+        
         //If the process image button is pressed
-        if(otherJoy.getRawButton(10)){
+        if(mainJoy.getRawButton(3)){
+            
             //Get a new image
-            targetFinder.processImage();
+            Target hightarget = targetFinder.processImage();
+            
+            //Prepare the wheels to shoot
+            upperShooter.prepareToShoot(hightarget.distance);
+        }
+        
+        //If the ramp up button is pressed
+        if(mainJoy.getRawButton(4)){
+            
+            try{
+                
+                //Set the motor powers
+                upperShooter.set();
+                
+            } catch (Exception ex){ //Catch all for errors
+
+                //Print Error
+                Debug.fatal(ex);
+
+            }
+            
+        }else{
+            
+            try{
+                
+                //Stop the motor
+                upperShooter.stopShooting();
+                
+            } catch (Exception ex){ //Catch all for errors
+
+                //Print Error
+                Debug.fatal(ex);
+
+            }
+            
+        }
+        
+        try {
+            
+            //If the shoot values are set
+            upperShooter.moveConveyorWithValue(otherJoy.getRawAxis(5));
+            
+        } catch (CANTimeoutException ex) {
+            
+            //Print Error
+            Debug.fatal(ex);
+            
         }
         
     }
@@ -66,24 +145,12 @@ public class OI {
         //Try catch for errors
         try {
             
-            //If the conveyor button is pressed
-            if(mainJoy.getRawButton(4)){
-                lowerConveyor.moveDown();
-            }else if(mainJoy.getRawButton(3)){
-                lowerConveyor.moveUp();
-            }else{
-                lowerConveyor.stop();
-            }
+            lowerConveyor.moveValue(otherJoy.getRawAxis(4));
             
-        } catch (CANTimeoutException ex) { //Catch CANTimeout Error
-            
-            //Print Error
-            Debug.fatal(ex, "CAN Timeout in " + this.getClass().getName());
-
         } catch (Exception ex){ //Catch all for errors
 
             //Print Error
-            Debug.fatal(ex, "Unknown error in " + this.getClass().getName());
+            Debug.fatal(ex);
 
         }
         
